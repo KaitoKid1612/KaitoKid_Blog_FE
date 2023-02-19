f99def967fcc1fdf30a477c33c8313ef9f926c12
KaitoKid1612 <leviet1612@gmail.com>
2023-02-19 11:49:41 +0700

    [#1-Feature1] Setup bootstrap and Layouts Admin

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container-fluid">
+        <div class="row align-items-center" style="background-color: green;padding: 1rem;">
+            <div class="col-12">
+                HEADER
+            </div>
+        </div>
+    </div>
+</template>
